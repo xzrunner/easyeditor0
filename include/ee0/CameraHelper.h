a8c884296cde22fd8d7b0d388c3a60ce43d3f13e
zz <zhuguang@ejoy.com>
2018-02-23 22:21:44 +0800

    rename

--- include/ee0/CameraHelper.h
+++ include/ee0/CameraHelper.h
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace s2 { class Camera; }
+namespace pt2 { class Camera; }
 
 #include <SM_Vector.h>
 
@@ -10,8 +10,8 @@ namespace ee0
 class CameraHelper
 {
 public:
-	static sm::vec2 TransPosScreenToProject(const s2::Camera& cam, int screen_x, int screen_y);
-	static sm::vec2 TransPosProjectToScreen(const s2::Camera& cam, const sm::vec2& project);
+	static sm::vec2 TransPosScreenToProject(const pt2::Camera& cam, int screen_x, int screen_y);
+	static sm::vec2 TransPosProjectToScreen(const pt2::Camera& cam, const sm::vec2& project);
 
 }; // CameraHelper
 
--- source/tools/CameraHelper.cpp
+++ source/tools/CameraHelper.cpp
@@ -1,30 +1,30 @@
 #include "ee0/CameraHelper.h"
 
-#include <sprite2/OrthoCamera.h>
+#include <painting2/OrthoCamera.h>
 #include <gum/RenderContext.h>
 
 namespace ee0
 {
 
-sm::vec2 CameraHelper::TransPosScreenToProject(const s2::Camera& cam, int screen_x, int screen_y)
+sm::vec2 CameraHelper::TransPosScreenToProject(const pt2::Camera& cam, int screen_x, int screen_y)
 {
-	if (cam.Type() != s2::CAM_ORTHO2D) {
+	if (cam.Type() != pt2::CAM_ORTHO2D) {
 		return sm::vec2(0, 0);
 	}
 
-	auto& ortho_cam = dynamic_cast<const s2::OrthoCamera&>(cam);
+	auto& ortho_cam = dynamic_cast<const pt2::OrthoCamera&>(cam);
 	int w = gum::RenderContext::Instance()->GetWidth(),
 		h = gum::RenderContext::Instance()->GetHeight();
 	return ortho_cam.TransPosScreenToProject(screen_x, screen_y, w, h);
 }
 
-sm::vec2 CameraHelper::TransPosProjectToScreen(const s2::Camera& cam, const sm::vec2& project)
+sm::vec2 CameraHelper::TransPosProjectToScreen(const pt2::Camera& cam, const sm::vec2& project)
 {
-	if (cam.Type() != s2::CAM_ORTHO2D) {
+	if (cam.Type() != pt2::CAM_ORTHO2D) {
 		return sm::vec2(0, 0);
 	}
 
-	auto& ortho_cam = dynamic_cast<const s2::OrthoCamera&>(cam);
+	auto& ortho_cam = dynamic_cast<const pt2::OrthoCamera&>(cam);
 	int w = gum::RenderContext::Instance()->GetWidth(),
 		h = gum::RenderContext::Instance()->GetHeight();
 	return ortho_cam.TransPosProjectToScreen(project, w, h);
