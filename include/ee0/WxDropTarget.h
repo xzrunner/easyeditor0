2e1322e821e6e0a7070595867e4f90ea0e6df946
zz <zhuguang@ejoy.com>
2018-02-26 11:44:35 +0800

    [REMOVED] OnDropSymbol()

--- include/ee0/WxDropTarget.h
+++ include/ee0/WxDropTarget.h
@@ -20,7 +20,6 @@ public:
 	//
 	virtual wxDragResult OnData(wxCoord x, wxCoord y, wxDragResult def) override;
 
-	virtual bool OnDropSymbol(const s2::SymPtr& sym, const sm::vec2& pos) { return false; }
 	virtual void OnDropText(wxCoord x, wxCoord y, const wxString& text) = 0;
 	virtual void OnDropFiles(wxCoord x, wxCoord y, const wxArrayString& filenames) = 0;
 
