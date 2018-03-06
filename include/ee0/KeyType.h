bda1c695f375b82494ed96e6b6ffd285620aaf8d
zz <zhuguang@ejoy.com>
2018-03-07 07:52:22 +0800

    [ADDED] key type

---
+++ include/ee0/KeyType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace ee0
+{
+
+static const int KEY_UP		= 0x01;
+static const int KEY_DOWN	= 0x02;
+static const int KEY_LEFT	= 0x03;
+static const int KEY_RIGHT	= 0x04;
+
+static const int KEY_SHIFT	= 0x10;
+static const int KEY_CTRL	= 0x20;
+static const int KEY_ALT	= 0x40;
+
+}
