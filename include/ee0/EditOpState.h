d1ac726794b928c397c5817407aef029bb75a1c5
zz <zhuguang@ejoy.com>
2019-02-23 20:51:14 +0800

    [ADDED] prev EditOpState

--- include/ee0/EditOpState.h
+++ include/ee0/EditOpState.h
@@ -14,30 +14,38 @@ public:
 		: m_camera(camera) {}
 	virtual ~EditOpState() {}
 
-	virtual void Bind() {}
-	virtual void UnBind() {}
+    virtual void Bind();
+    virtual void UnBind();
 
-	virtual bool OnKeyPress(int key_code) { return false; }
-	virtual bool OnKeyRelease(int key_code) { return false; }
-	virtual bool OnMousePress(int x, int y) { return false; }
-	virtual bool OnMouseRelease(int x, int y) { return false; }
-	virtual bool OnMouseMove(int x, int y) { return false; }
-	virtual bool OnMouseDrag(int x, int y) { return false; }
+    virtual bool OnKeyPress(int key_code);
+    virtual bool OnKeyRelease(int key_code);
+    virtual bool OnMousePress(int x, int y);
+    virtual bool OnMouseRelease(int x, int y);
+    virtual bool OnMouseMove(int x, int y);
+    virtual bool OnMouseDrag(int x, int y);
 
-	virtual bool OnDirectionKeyDown(int type) { return false; }
+    virtual bool OnDirectionKeyDown(int type);
 
-	virtual bool OnMouseWheelRotation(int x, int y, int direction) { return false; }
+    virtual bool OnMouseWheelRotation(int x, int y, int direction);
 
-	virtual bool OnActive(bool active) { return false; };
-	virtual bool OnDraw() const { return false; };
-	virtual bool Clear() { return false; }
+	virtual bool OnActive(bool active);
+	virtual bool OnDraw() const;
+    virtual bool Clear();
 
 	void SetCamera(const std::shared_ptr<pt0::Camera>& camera) {
 		m_camera = camera;
 	}
 
+    auto& SetPrevOpState(const std::shared_ptr<EditOpState>& st) {
+        m_prev_st = st;
+        return st;
+    }
+
 protected:
 	std::shared_ptr<pt0::Camera> m_camera = nullptr;
+
+private:
+    std::shared_ptr<EditOpState> m_prev_st = nullptr;
 
 }; // EditOpState
 
---
+++ source/editop/EditOpState.cpp
@@ -0,0 +1,119 @@
+#include "ee0/EditOpState.h"
+
+namespace ee0
+{
+
+void EditOpState::Bind()
+{
+    if (m_prev_st) {
+        m_prev_st->Bind();
+    }
+}
+
+void EditOpState::UnBind()
+{
+    if (m_prev_st) {
+        m_prev_st->UnBind();
+    }
+}
+
+bool EditOpState::OnKeyPress(int key_code)
+{
+	if (m_prev_st && m_prev_st->OnKeyPress(key_code)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnKeyRelease(int key_code)
+{
+	if (m_prev_st && m_prev_st->OnKeyRelease(key_code)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnMousePress(int x, int y)
+{
+	if (m_prev_st && m_prev_st->OnMousePress(x, y)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnMouseRelease(int x, int y)
+{
+	if (m_prev_st && m_prev_st->OnMouseRelease(x, y)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnMouseMove(int x, int y)
+{
+	if (m_prev_st && m_prev_st->OnMouseMove(x, y)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnMouseDrag(int x, int y)
+{
+	if (m_prev_st && m_prev_st->OnMouseDrag(x, y)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnDirectionKeyDown(int type)
+{
+	if (m_prev_st && m_prev_st->OnDirectionKeyDown(type)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnMouseWheelRotation(int x, int y, int direction)
+{
+	if (m_prev_st && m_prev_st->OnMouseWheelRotation(x, y, direction)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnActive(bool active)
+{
+	if (m_prev_st && m_prev_st->OnActive(active)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::OnDraw() const
+{
+	if (m_prev_st && m_prev_st->OnDraw()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool EditOpState::Clear()
+{
+	if (m_prev_st && m_prev_st->Clear()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+}
