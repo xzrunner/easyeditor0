2fe7e0691bbe300edfa83745df57ff6936667463
zz <zhuguang@ejoy.com>
2019-03-06 07:13:27 +0800

    [FIXED] should use const char*

--- include/ee0/Variant.h
+++ include/ee0/Variant.h
@@ -16,19 +16,19 @@ public:
 
 	union
 	{
-		int8_t   b;              // VT_BYTE
-		int16_t  i;              // VT_SHORT
-		int32_t  l;              // VT_LONG
-		int64_t  ll;             // VT_LONGLONG
-		float    flt;            // VT_FLOAT
-		double   dbl;            // VT_DOUBLE
-		char     c;              // VT_CHAR
-		uint16_t ui;             // VT_USHORT
-		uint32_t ul;             // VT_ULONG
-		uint64_t ull;            // VT_ULONGLONG
-		bool     bl;             // VT_BOOL
-		char*    pc;             // VT_PCHAR
-		void*    pv;             // VT_PVOID
+		int8_t      b;              // VT_BYTE
+		int16_t     i;              // VT_SHORT
+		int32_t     l;              // VT_LONG
+		int64_t     ll;             // VT_LONGLONG
+		float       flt;            // VT_FLOAT
+		double      dbl;            // VT_DOUBLE
+		char        c;              // VT_CHAR
+		uint16_t    ui;             // VT_USHORT
+		uint32_t    ul;             // VT_ULONG
+		uint64_t    ull;            // VT_ULONGLONG
+		bool        bl;             // VT_BOOL
+		const char* pc;             // VT_PCHAR
+		void*       pv;             // VT_PVOID
 	} m_val;
 
 }; // Variant
