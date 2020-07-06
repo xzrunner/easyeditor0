e6862079883495633e0534a44cb8ac7563d6df05
zz <xzrunner@gmail.com>
2020-07-07 05:30:35 +0800

    [FIXED] some problems after press ok

--- source/gui/WxEditCodeProp.cpp
+++ source/gui/WxEditCodeProp.cpp
@@ -17,19 +17,16 @@ bool WxEditCodeProp::OnButtonClick(wxPropertyGrid* propgrid, wxString& value)
     WxCodeEditDlg dlg(m_parent, m_code_str);
     dlg.SetCallback(m_cb);
 
-	if (dlg.ShowModal() == wxID_OK)
-	{
+	if (dlg.ShowModal() == wxID_OK) {
         m_code_str = dlg.GetCodeStr();
-		SetValue(m_code_str);
-		//if (m_cb) {
-		//	m_cb(m_code_str);
-		//}
-		return true;
 	}
-	else
-	{
-		return false;
+
+	SetValue(m_code_str);
+	if (m_cb) {
+		m_cb(m_code_str);
 	}
+
+    return false;
 }
 
 }
